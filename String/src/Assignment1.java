2718f6389a397b4abfe68fa88cf84eaae14bf2f1
kanishka2910 <56442094+kanishka2910@users.noreply.github.com>
2021-01-05 12:37:57 +0530

    Add files via upload

---
+++ String/src/Assignment1.java
@@ -0,0 +1,23 @@
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		String str1 = "Hello Dear Friend";
+		//Output as  :  Friends Dear Hello
+		String output = reverseString(str1);
+		//System.out.println(output);
+		
+
+	}
+
+	private static String reverseString(String input) {
+		String [] tokens = input.split(" ");
+		for(int i=tokens.length-1;i>=0;i--)
+		{
+			System.out.print(" "+tokens[i]);
+		}
+		
+		
+		return null;
+	}
+
+}
